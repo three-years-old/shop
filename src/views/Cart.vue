efd4843b445c9758ed2cbccc8e872c0fa7e3f84a
Chen Chen <15767557577@163.com>
2019-12-05 01:08:05 +0800

    init shop

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>123</div>
+  <div>购物车</div>
 </template>
 <script>
 export default {};
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>123</div>
+  <div>我的</div>
 </template>
 <script>
 export default {};
