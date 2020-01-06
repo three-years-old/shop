0bb05f62e73a600de496255926cd5fdac6efb4e9
Chen Chen <15767557577@163.com>
2020-01-06 15:08:48 +0800

    第5次提交

--- src/service.config.js
+++ src/service.config.js
@@ -11,6 +11,9 @@ const MOCKURL = 'http://www.a123.com/';
 const SERVERURL = "http://localhost:3000/";
 const URL = {
     registUser: SERVERURL + 'user/registUser',
-    loginUser: SERVERURL + 'user/loginUser'
+    loginUser: SERVERURL + 'user/loginUser',
+    getTypes: SERVERURL + 'type/getTypes',
+    getProductsByType: SERVERURL + 'product/getProductsByType'
+    
 }
 export default URL
